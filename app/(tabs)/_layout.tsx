7055a9905d6cebc9c120fcff759186a273ec1a62
DebasishDebnath <debasish.debnath@iemlabs.com>
2024-11-13 18:21:36 +0530

    basic routing and navigating

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,34 +1,64 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+// import { Link, Slot } from "expo-router";
+// import { Button, Text, View } from "react-native";
+// import { SafeAreaView } from "react-native-safe-area-context";
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+// export default function Layout() {
+//   return (
+//     <SafeAreaView>
+//       <View style={{ height: "90%" }}>
+//         <Slot />
+//       </View>
+//       <View style={{ backgroundColor: "red" }}>
+//         <Link href={"/explore"}>
+//           <Text>explore page</Text>
+//         </Link>
+//         <Link href={"/"}>
+//           <Text>for you page</Text>
+//         </Link>
+//         <Link href={"/account"}>
+//           <Text>account page</Text>
+//         </Link>
+//       </View>
+//     </SafeAreaView>
+//   );
+// }
+
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Tabs } from "expo-router";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: "blue",
         headerShown: false,
-      }}>
+      }}
+    >
+      <Tabs.Screen
+        name="account"
+        options={{
+          title: "Account",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="home" color={color} />
+          ),
+        }}
+      />
+
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+          title: "For You",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="cog" color={color} />
           ),
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+          title: "Explore",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="cog" color={color} />
           ),
         }}
       />
