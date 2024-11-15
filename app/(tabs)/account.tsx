781514f106d92bb8f7e880940b7f53cb672b9536
DebasishDebnath <debasish.debnath@iemlabs.com>
2024-11-15 11:46:20 +0530

    bottom sheet

--- app/(tabs)/account.tsx
+++ app/(tabs)/account.tsx
@@ -1,10 +1,24 @@
-import { Text, View } from "react-native";
+import { DownloadPicture } from "@/components/BottomSheet";
+import { useState } from "react";
+import { Button, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function account() {
+  const [pictureopen, setPictureopen] = useState(false);
   return (
-    <SafeAreaView>
-      <Text>account</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1 }}>
+        <Text>account</Text>
+        <Button
+          title="Open Bottom Sheet"
+          onPress={() => {
+            setPictureopen(true);
+          }}
+        ></Button>
+        {pictureopen && (
+          <DownloadPicture onClose={() => setPictureopen(false)} />
+        )}
+      </View>
     </SafeAreaView>
   );
 }
